9e165a1eedf717889660803e0421b47549aa4937
Ahmad Hafizh Assaad <71488@fif.co.id>
2025-06-13 15:40:49 +0700

    :fire: ERRORRRRR

--- hotel-booking-api/src/main/java/com/example/hotel_booking_api/repository/BookingRepository.java
+++ hotel-booking-api/src/main/java/com/example/hotel_booking_api/repository/BookingRepository.java
@@ -1,12 +1,12 @@
-package com.example.hotel_booking_api.repository;
-
-import com.example.hotel_booking_api.model.Booking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.time.LocalDate;
-import java.util.List;
-
-public interface BookingRepository extends JpaRepository<Booking, Long> {
-    List<Booking> findByCheckInBetween(LocalDate start, LocalDate end);
-    List<Booking> findByRoomId(Long roomId);
-}
+//package com.example.hotel_booking_api.repository;
+//
+//import com.example.hotel_booking_api.model.Booking;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//import java.time.LocalDate;
+//import java.util.List;
+//
+//public interface BookingRepository extends JpaRepository<Booking, Long> {
+//    List<Booking> findByCheckInBetween(LocalDate start, LocalDate end);
+//    List<Booking> findByRoomId(Long roomId);
+//}
--- hotel-booking-api/src/main/java/com/example/hotel_booking_api/validation/CheckInBeforeCheckOut.java
+++ hotel-booking-api/src/main/java/com/example/hotel_booking_api/validation/CheckInBeforeCheckOut.java
@@ -1,16 +1,15 @@
-// validation/CheckInBeforeCheckOut.java
-package com.example.hotel_booking_api.validation;
-
-import jakarta.validation.Constraint;
-import jakarta.validation.Payload;
-import java.lang.annotation.*;
-
-@Documented
-@Constraint(validatedBy = CheckInBeforeCheckOutValidator.class)
-@Target({ ElementType.TYPE })
-@Retention(RetentionPolicy.RUNTIME)
-public @interface CheckInBeforeCheckOut {
-    String message() default "Check-in date must be before check-out date";
-    Class<?>[] groups() default {};
-    Class<? extends Payload>[] payload() default {};
-}
+//package com.example.hotel_booking_api.validation;
+//
+//import jakarta.validation.Constraint;
+//import jakarta.validation.Payload;
+//import java.lang.annotation.*;
+//
+//@Documented
+//@Constraint(validatedBy = CheckInBeforeCheckOutValidator.class)
+//@Target({ ElementType.TYPE })
+//@Retention(RetentionPolicy.RUNTIME)
+//public @interface CheckInBeforeCheckOut {
+//    String message() default "Check-in date must be before check-out date";
+//    Class<?>[] groups() default {};
+//    Class<? extends Payload>[] payload() default {};
+//}
